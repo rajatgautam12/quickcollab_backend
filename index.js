0b842f9b3477fe8cc17b0f7b042b06f8d9412040
Rajat <rg8441626@gmail.com>
2025-05-08 15:22:43 +0530

    fsdfs

--- index.js
+++ index.js
@@ -233,6 +233,6 @@ io.on('connection', (socket) => {
 });
 
 // const PORT = process.env.PORT || 5000;
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+server.listen(5000, () => {
+  console.log(`Server running on port 5000`);
+});
